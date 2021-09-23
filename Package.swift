8f97cbd836736dffcab3adc9783f313ef4726092
Tony Dang <tuong.dang@axonactive.com>
2021-09-23 16:19:27 +0700

    Update Package file

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "1.0.1"
+let version = "1.0.0"
 
 let package = Package(
     name: "VibeBuildXCFramework",
@@ -73,53 +73,53 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GoogleMaps.xcframework.zip",
-            checksum: "742010ab01c5225eb30e0c3009c921c7c9dde68649f0925053d12f597d4f76c7"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GoogleMaps.xcframework.zip",
+            checksum: "6b6b0add8da03d2b1b004a18affe96f6538dd2444ab379d0d623947aa9b872d5"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GoogleMapsBase.xcframework.zip",
-            checksum: "52bc34a9f2d837c76d0464460c265f8230fb4457c1a97c1413cb8a233bc879a1"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GoogleMapsBase.xcframework.zip",
+            checksum: "80ed78c44ce128381e975e0bb7d6f3ced36e63059a557e72c81d6513cff65116"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GoogleMapsCore.xcframework.zip",
-            checksum: "d6b7647750c646f25eb2d66bdccdbd3490e2d81cfa059510f103c2e8d9cd0d66"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GoogleMapsCore.xcframework.zip",
+            checksum: "13ddd0bd4fbda7152b34f9c7236de70f2a85a518c41717b227de040c8ad31db8"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GoogleMapsM4B.xcframework.zip",
-            checksum: "21eeb5cef14b82a63d7f42f7bbfd6d5fde71b19c5a64f6359bef066310d44ccb"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GoogleMapsM4B.xcframework.zip",
+            checksum: "c6b76521e6c7ad6ea4eaff980c501258e6001a40ea8d8ef1975cdc6ee0d8abc9"
         ),
         .binaryTarget(
             name: "GooglePlaces",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GooglePlaces.xcframework.zip",
-            checksum: "00e8a87ffa529dc9657c254064b3b42de06645556a0d60e892980822e159ffd1"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GooglePlaces.xcframework.zip",
+            checksum: "37cd3b9f678e6c60ddb94c46eb1a32c0de8604cc228452599941707fffa84868"
         ),
         .binaryTarget(
             name: "Smooch",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/Smooch.xcframework.zip",
-            checksum: "96246fbae1cd61173872c00e3ff14270fb04adee539c96027357d372c45adedf"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/Smooch.xcframework.zip",
+            checksum: "4471fda58a7bf598cfe679fca2c09fe9e849b0941c2be7438b7fef9870af7a1d"
         ),
         .binaryTarget(
             name: "AppAuth",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/AppAuth.xcframework.zip",
-            checksum: "f9ca252870987ac27c2cf060d7fb5922fefd00ceee5853bbc90b8976b0662195"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/AppAuth.xcframework.zip",
+            checksum: "e231f176bebf7aedc9107c56858f7d51197ab561a2b9c8d0b8a1c652b1173e2c"
         ),
         .binaryTarget(
             name: "GoogleSignIn",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GoogleSignIn.xcframework.zip",
-            checksum: "22d48388b89a0feecf8d59bb1a34bb20023ef062b2562378f5c0a867d8f67334"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GoogleSignIn.xcframework.zip",
+            checksum: "e3cd7d581106fb79ba3a11890db3d7878decb8689a77ff682b0bc80a967a578b"
         ),
         .binaryTarget(
             name: "GTMAppAuth",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GTMAppAuth.xcframework.zip",
-            checksum: "62772bf11058502ac1e9dad17744cecf86e907deb7111aa814a09b16ec42ece0"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GTMAppAuth.xcframework.zip",
+            checksum: "6f2c97520b93cb59cfcd06da5d960192374dbb0a33820753c97164a5bfeb373b"
         ),
         .binaryTarget(
             name: "GTMSessionFetcher",
-            url: "https://gitlab.com/tuongdang/vibe-ios-build-xcframework/-/tree/master/releases/download/\(version)/GTMSessionFetcher.xcframework.zip",
-            checksum: "99234f5ff2cc719e4dec2b2ea7807f32cdf52a1d9e83b459a73b548dcca8a97f"
+            url: "https://github.com/tonytuong/vibe-ios-build-xcframework/releases/download/v1.0.0/GTMSessionFetcher.xcframework.zip",
+            checksum: "53ba88cf81e2b9a41aafc9b3a93263d0d433752b71a15737ca3a5f90d6fc2129"
         )
     ]
 )
